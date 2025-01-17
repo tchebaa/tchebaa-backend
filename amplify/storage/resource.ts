20f24ba6535809e729d52c41cc9d870058c909ad
stephen fondo <fondolski@gmail.com>
2025-01-17 13:16:25 +0300

    added storage permission

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,5 +2,15 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'tchebaaDrive',
+  access: (allow) => ({
+    'profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
 });
 
