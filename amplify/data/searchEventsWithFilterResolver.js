595c0062b70093322b8cf222b4c9fab089a7753c
stephen fondo <fondolski@gmail.com>
2025-01-19 13:31:45 +0300

    edited location query

--- amplify/data/searchEventsWithFilterResolver.js
+++ amplify/data/searchEventsWithFilterResolver.js
@@ -7,57 +7,41 @@ import { util } from "@aws-appsync/utils";
  */
 export function request(ctx) {
 
-    if(ctx.args.categories && ctx.args.categories.length > 0 ) {
-
-        if(ctx.args.searchTerm && ctx.args.searchTerm.length > 1) {
-
-        } else {
-
-        }
-
-
-    } else {
-
-         if(ctx.args.searchTerm && ctx.args.searchTerm.length > 1) {
-
-         } else {
-            return {
-                operation: "GET",
-                path: "/event/_search",
-                params: {
-                    body: {
+    return {
+        operation: "GET",
+        path: "/event/_search",
+        params: {
+            body: {
+                query: {
+                    bool: {
+    
+                        filter: [{
+                            geo_distance: {
+                            distance: "200km",
+                            location: {
+                                lat: ctx.args.latitude,
+                                lon: ctx.args.longitude
+                            }
+                            }
+                        }]
+    
+                    },
+                    nested: {
+                        path: "dateTimePriceList",
                         query: {
-                            bool: {
-            
-                                filter: [{
-                                    "geo_distance": {
-                                    "distance": "200km",
-                                    "location": {
-                                        "lat": ctx.args.latitude,
-                                        "lon": ctx.args.longitude
-                                    }
-                                    }
-                                }]
-            
-                            },
-                            nested: {
-                                path: "dateTimePriceList",
-                                query: {
-                                    bool:{
-                                        must: [
-                                            { range: { "dateTimePriceList.eventEndDate": { "lte": ctx.args.endDate} } }
-                                        ]
-                                        
-                                    }
-                                }
+                            bool:{
+                                must: [
+                                    { range: { "dateTimePriceList.eventEndDate": { "lte": ctx.args.endDate} } }
+                                ]
+                                
                             }
                         }
                     }
                 }
-                
-              };
-         }
-    }
+            }
+        }
+        
+      };
 
 
  
